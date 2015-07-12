4303a2497ae444ae9628354364bfe23e02598165
Nathan Prevette <nprevette420@gmail.com>
2015-07-12 10:14:42 -0400

    Build Viper not AudioFX

--- bliss.mk
+++ bliss.mk
@@ -4,6 +4,9 @@ $(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/bliss/config/nfc_enhanced.mk)
 
+#Bliss Viper Audio Mod
+BLISS_AUDIO_MOD := viper
+
 $(call inherit-product, device/sony/togari/full_togari.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6833 BUILD_FINGERPRINT=Sony/C6833/C6833:5.0.2/14.5.A.0.270/3305956307:user/release-keys PRIVATE_BUILD_DESC="C6833-user 5.0.2 14.5.A.0.270 3305956307 release-keys"
